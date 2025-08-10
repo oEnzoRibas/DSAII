c0090bf225b7517628016b0128336ab82d408cae
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-08-09 22:28:02 -0300

    Add program to print multiples of 3 or 5 below 100
    Introduced Main.java which defines a method to collect and print all numbers less than 100 that are multiples of 3 or 5. This provides a simple example of using ArrayList and basic control flow in Java.

---
+++ src/main/java/Lists/L1/Exs/two/Main.java
@@ -0,0 +1,29 @@
+package Lists.L1.Exs.two;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Integer n = 3, m = 5;
+
+        ArrayList<Integer> result;
+
+        result = multiples(n,m);
+
+        for (Integer i : result){
+            System.out.println(i);
+        }
+
+    }
+
+    public static ArrayList<Integer> multiples(Integer n, Integer m) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for (Integer i = 0; i < 100; i++) {
+            if ((i % n == 0) || (i % m == 0) ){
+                resultList.add(i);
+            }
+        }
+        return resultList;
+    }
+}
