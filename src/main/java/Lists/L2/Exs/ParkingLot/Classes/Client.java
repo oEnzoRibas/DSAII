d1dd248ff20a2d23d4a6b4ab5338f6abe67e75fd
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-08-24 14:25:32 -0300

    Refactor Client to extend People, add People model
    Refactored Client class to inherit from new People model for better code reuse and encapsulation. Added People.java to represent shared attributes. Removed redundant fields and accessors from Client. Added answers.md with project instructions and OOP concepts. Deleted .keep file from ParkingLot directory.

--- src/main/java/Lists/L2/Exs/ParkingLot/Classes/Client.java
+++ src/main/java/Lists/L2/Exs/ParkingLot/Classes/Client.java
@@ -1,13 +1,12 @@
 package Lists.L2.Exs.ParkingLot.Classes;
 
-public class Client {
-    private int cpf;
-    private String name;
+import Lists.L2.Exs.ParkingLot.Classes.Models.People;
+
+public class Client extends People {
     private Date dateRegistration;
 
     public Client(int cpf, String name) {
-        this.cpf = cpf;
-        this.name = name;
+        super(cpf, name);
         this.dateRegistration = new Date(true);
     }
 
@@ -15,22 +14,6 @@ public class Client {
     public String toString() {
         return "Client \n CPF: " + cpf + "\n Name: " + name +
                 "\n Date of Registration: " + dateRegistration.getDate();
-    }
-
-    public int getCpf() {
-        return cpf;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public void setDateRegistration(Date dateRegistration) {
