c7ba9dec2bbbec3b691ec6a47ff4e3900527f973
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-09-11 23:49:08 -0300

    Remove main.java and fix sum logic in soma.java
    Deleted main.java and updated soma.java to correct the recursive sum logic and change the input value from 123 to 421.

--- src/main/java/Course/Tests/p1_2501/C/main.java
+++
@@ -1,7 +0,0 @@
-package Course.Tests.p1_2501.C;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println("helo");
-    }
-}
--- src/main/java/Course/Tests/p1_2501/C/soma.java
+++ src/main/java/Course/Tests/p1_2501/C/soma.java
@@ -2,7 +2,7 @@ package Course.Tests.p1_2501.C;
 
 public class soma {
     public static void main(String[] args) {
-        int a = somador(123);
+        int a = somador(421);
         System.out.println(a);
     }
 
@@ -10,6 +10,6 @@ public class soma {
         if (i == 0){
             return 0;
         }
-        return i / 10 + somador(i%10);
+        return i % 10 + somador(i / 10);
     }
 }
