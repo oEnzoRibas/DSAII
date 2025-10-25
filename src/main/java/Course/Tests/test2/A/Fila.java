46e60213c4e106613ad9d6c159005836a0c98efd
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-10-24 21:56:04 -0300

    Add Fila and CelulaInt classes for queue operations
    Introduced the CelulaInt and Fila classes to implement a queue with integer elements, including methods for enqueue, dequeue, size, sum, average, selection sort, binary search, and a special 'furafila' operation. Updated compiler settings to enable preview features for the 'dsaii' module. Added a related binary file A.rar.

---
+++ src/main/java/Course/Tests/test2/A/Fila.java
@@ -0,0 +1,124 @@
+package A;
+
+public class Fila {
+    CelulaInt first, last;
+    int tam;
+
+
+    public Fila() {
+        this.first = null;
+        this.last = null;
+        this.tam = 0;
+    }
+
+    public void enfileirar(int x) {
+        CelulaInt tmp = new CelulaInt(x);
+        if (tam == 0) {
+            last = tmp;
+            first = tmp;
+        } else {
+            this.last.setProx(tmp);
+            last = last.getProx();
+
+        }
+        tam++;
+    }
+
+    public int removerDaFila() {
+        int element = this.first.getElemento();
+        this.first = this.first.getProx();
+        tam--;
+        return element;
+    }
+
+    public int tamanhoDaFila() {
+        return this.tam;
+    }
+
+    public void mostrarfila() {
+
+        System.out.println("[");
+        for (CelulaInt c = this.first; c != null; c = c.getProx()) {
+            System.out.println(c.getElemento());
+        }
+        System.out.println("]");
+    }
+
+    public double media() {
+        return (double) this.soma() / this.tam;
+    }
+
+    public int soma() {
+        int soma = 0;
+        for (CelulaInt c = this.first; c != null; c = c.getProx()) {
+            soma += c.getElemento();
+        }
+        return soma;
+    }
+
+    public void furafila(int e){
+
+        CelulaInt ant= first;
+        CelulaInt pos = ant.getProx();
+        for (CelulaInt c = this.first; c != null; c = c.getProx()){
+            if(c.getProx().getElemento() == e){
+                ant = c;
+                pos = c.getProx();
+                break;
+            }
+
+        }
+
+
+
+        ant.setProx(pos.getProx());
+        pos.setProx(this.first);
+        this.first = pos;
+
+
+
+    }
+
+    public boolean binarySearch(int key){
+        this.selectionSort();
+        CelulaInt pri = this.first, ult = last;
+        while(pri.getElemento() < ult.getElemento()) {
+
+            CelulaInt midCell = pri;
+
+            // pega celula do meio
+            for (int i = 0; i <= tam / 2; i++) {
+                midCell = midCell.getProx();
+            }
+
+            if (midCell.getElemento() == key) {
+                return true;
+            }
+
+            if (key > midCell.getElemento()){
+                pri = midCell.getProx();
+            }
+            pri = pri.getProx();
+
+        }
+        return false;
+    }
+
+    public void selectionSort() {
+        for (CelulaInt c = this.first; c != null; c = c.getProx()) {
+            CelulaInt minCell = this.first;
+            int min = this.first.getElemento();
+            for (CelulaInt d = c.getProx(); d != null; d = d.getProx()) {
+                if (d.getElemento() < min){
+                    min = d.getElemento();
+                    minCell = d;
+            }
+                if (c.getElemento() > min){
+                    int tmp = c.getElemento();
+                    c.setElemento(min);
+                    minCell.setElemento(tmp);
+                }
+        }
+    }
+}
+}
