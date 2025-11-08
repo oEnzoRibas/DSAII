907cb60f811a53ed7c1bcffc066a4d050028ef72
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-11-07 21:04:30 -0300

    add linear list

---
+++ src/main/java/Course/Lists/L8/A/LinearList.java
@@ -0,0 +1,19 @@
+public class LinearList<T>{
+	private T[] array;
+	private int n;
+	
+
+	public void LinearList(){
+		this.n = 0;
+	}
+
+	public void insertStart(){
+		
+	}
+	public static void main(String args[]){
+
+	
+}
+
+}
+
